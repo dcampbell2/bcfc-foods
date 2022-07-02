551292c3616db747132b18e65dba377003afc3c8
Damian Campbell <damiancampbell@Damian-ka-MBP.attlocal.net>
2022-07-02 12:07:04 -0400

    adding nav hamburger

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -1,21 +1,62 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Offcanvas from "react-bootstrap/Offcanvas";
 import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button"
 
 const NavBar = () => {
   return (
-    <div>
-      <Nav className="justify-content-center" activeKey="/">
-        <Nav.Item>
-          <Nav.Link href="/">Home</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/izlandgirls">Izland Girls</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/reevesbbq">Reeves Souther BBQ</Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </div>
+<>
+    <Navbar key={false} bg="light" expand={false} className="mb-3">
+      <Container fluid>
+        <Navbar.Brand href="#">Foods of BCFC</Navbar.Brand>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-false}`} />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-false`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-false`}
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-false`}>
+              Offcanvas
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Link</Nav.Link>
+              <NavDropdown
+                title="Dropdown"
+                id={`offcanvasNavbarDropdown-expand-false`}
+              >
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+</>
   );
 };
 
