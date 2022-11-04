a77bba3c2f52366167c7b9ab3483a7250e5df196
Damian Campbell <damiancampbell@Damian-ka-MBP.attlocal.net>
2022-11-04 08:19:48 -0400

    adding waves design to home page

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,8 @@ import Home from "./Containers/Home/Home";
 import Jamaican from "./Containers/Food/Jamaican";
 import Southern from "./Containers/Food/Southern";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import AdminPage from "./Containers/Admin/AdminPage";
 
 function App() {
   useEffect(() => {
@@ -20,6 +20,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/izlandgirls" component={Jamaican} />
         <Route exact path="/reevesbbq" component={Southern} />
+        <Route exact path="/admin" component={AdminPage} />
       </Switch>
     </Router>
   );
