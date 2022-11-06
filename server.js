3eb7da507f411bcf2d7de45390a08ea00ee25115
Damian Campbell <damiancampbell@Damian-ka-MacBook-Pro.local>
2022-11-06 06:28:37 -0500

    attempting to fix heroku deployment issue

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path")
+const path = require("path");
 const app = express();
-const IzlandController = require("./controllers/izland")
+const IzlandController = require("./controllers/izland");
 
 const PORT = process.env.PORT || 3001;
 
@@ -28,16 +28,16 @@ connection.on("error", (err) => {
   console.log("Mongoose connection error", err);
 });
 
-app.use("/api/izlandgirls", IzlandController)
+app.use("/api/izlandgirls", IzlandController);
 
 app.get("/api/config", (req, res) => {
   res.json({ success: true });
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
